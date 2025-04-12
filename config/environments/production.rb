1d07e897388ecf414c7a5b2779e4289490546e55
emareaitch <dev.emareaitch@gmail.com>
2025-04-12 10:41:43 +0530

    Added email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.assets.css_compressor = nil
+  config.action_mailer.default_url_options = { host: 'echo-it.onrender.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'stridescholar.onrender.com', # Update this to match your Render domain
+    user_name: Rails.application.credentials.gmail_username,
+    password: Rails.application.credentials.gmail_password,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
