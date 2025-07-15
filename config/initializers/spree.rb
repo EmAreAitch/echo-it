9b0b0c8d0385a6b8dc2b5bb9e2bf39740cb9a63b
emareaitch <dev.emareaitch@gmail.com>
2025-07-15 12:26:20 +0530

    Updated price to USD

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -7,7 +7,7 @@ Spree.load_defaults "4.5.0"
 Spree.config do |config|
   # Core:
   # Default currency for new sites
-  config.currency = Rails.env.development? ? "USD" : "INR"
+  config.currency = "USD"
 
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
